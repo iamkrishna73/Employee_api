88811a65b75933f3bd8e371086fe909d95c5b5f1
krishna <krishnasingj137333@gmail.com>
2023-10-06 00:40:07 +0530

    jdbc auth

--- src/main/java/com/example/EmployeeApi/Security/EmployeeSecurityConfiguration.java
+++ src/main/java/com/example/EmployeeApi/Security/EmployeeSecurityConfiguration.java
@@ -8,12 +8,39 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class EmployeeSecurityConfiguration {
 
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer->
+                configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+        //use HTTP basic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        //Disable CSRF site Request
+        httpSecurity.csrf(csrf-> csrf.disable());
+
+        return httpSecurity.build();
+    }
+
+  /*  @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
                 .username("john")
@@ -34,24 +61,6 @@ public class EmployeeSecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(configurer->
-                configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-        );
-        //use HTTP basic authentication
-        httpSecurity.httpBasic(Customizer.withDefaults());
-
-        //Disable CSRF site Request
-        httpSecurity.csrf(csrf-> csrf.disable());
-
-        return httpSecurity.build();
-    }
+    } */
 
 }
